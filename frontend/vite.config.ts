71a0ce0d2478d0f0f86664fcd8179586f1d01b12
nancykaur11 <nancykaur1119@gmail.com>
2025-05-09 14:21:35 +0530

    Build ready - output to dist

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	server: {
-		port: 5175
-	},
-	plugins: [react()],
-	build: {
-		outDir: 'build'
-	}
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+	chunkSizeWarningLimit: 1000
+  }
 });
